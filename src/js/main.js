38a870a8a64015560ccf46334e875fa3b71a551a
Sergh1994 <sergijantoniuk123@gmail.com>
2022-01-18 11:16:25 +0200

    add adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,13 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
 });
+
+(function(){
+  const burger = document?.querySelector('[data-burger]');
+  const menu = document?.querySelector('[data-menu]');
+
+  burger?.addEventListener('click', (e) => {
+    burger?.classList.toggle('burger--active');
+    menu?.classList.toggle('menu--active');
+  });
+})();
